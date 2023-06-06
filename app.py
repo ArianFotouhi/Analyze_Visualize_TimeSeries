2978b086309746af42653c5894fe023e8f07ea20
Arian <78182876+ArianFotouhi@users.noreply.github.com>
2023-06-06 17:54:49 -0400

    v2.4

--- app.py
+++ app.py
@@ -1,98 +1,70 @@
-from flask import Flask, render_template, request, redirect, url_for, session, jsonify
-# import matplotlib.pyplot as plt
+from flask import Flask, render_template, request, redirect, session, jsonify
 import pandas as pd
-import json
 
 app = Flask(__name__)
 app.secret_key = "!241$gc"
 
 users = {
-    "user1":"pass1",
-    "user2":"pass2"
+    "user1": {"password": "pass1", "data_range": ("2006-01-01", "2010-12-31")},
+    "user2": {"password": "pass2", "data_range": ("2010-01-01", "2015-12-31")},
+    "user3": {"password": "pass3", "data_range": ("2015-01-01", "2022-12-31")}
 }
 
-
-# df = pd.read_csv("data/AAL.csv")
-
-
-#Authentication
 def authenticate(username, password):
-    if username in users and users[username] == password:
+    if username in users and users[username]["password"] == password:
         return True
     return False
 
-def data_render(to_dict=True):
-    # time_series_data = [
-    #     {'Date': '2022-01-01', 'Volume': 3},
-    #     {'Date': '2022-01-02', 'Volume': 15},
-    #     {'Date': '2022-01-03', 'Volume': 12},
-    #     {'Date': '2022-01-04', 'Volume': 14},
-    #     {'Date': '2022-01-05', 'Volume': 25},
-    #     {'Date': '2022-01-06', 'Volume': 8},
-    #     ]
-    
-    
+def load_data():
     df = pd.read_csv("data/AAL.csv")
-    categories = df['Country'].unique().tolist()
-    
-    if to_dict:
-        print(df.head())
-        df = df.to_dict(orient='records')
-    
-    return df,categories
+    df['Date'] = pd.to_datetime(df['Date'])
+    return df
 
-#Routes
+def filter_data(username, df, selected_country):
+    if selected_country:
+        filtered_df = df[df['Country'] == selected_country]
+    else:
+        data_range = users[username]["data_range"]
+        filtered_df = df[(df['Date'] >= data_range[0]) & (df['Date'] <= data_range[1])]
+    return filtered_df
 
-#Home
 @app.route('/', methods=['GET'])
 def index():
     if 'username' in session:
         return redirect('/visual')
     return redirect('/login')
 
-
-
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     if request.method == 'POST':
         username = request.form['username']
         password = request.form['password']
-        
         if authenticate(username, password):
-            session["username"] = username      
+            session["username"] = username
             return redirect('/visual')
         else:
             return render_template("login.html", error="Invalid username or password")
     return render_template("login.html")
-    
-  
 
-    
-#Visualization
-@app.route('/visual', methods=['GET','POST'])
+@app.route('/visual', methods=['GET'])
 def visual():
     if 'username' not in session:
-
         return redirect('/login')
 
-    data, categories = data_render(to_dict=True)
-
-            
-    # Pass the time series data to the template
+    selected_country = request.args.get('country', '')
+    df = load_data()
+    filtered_df = filter_data(session["username"], df, selected_country)
     
-    return render_template('visual.html', data=data, countries=categories)
+    data = filtered_df.to_dict(orient='records')
+    categories = df['Country'].unique().tolist()
 
-# Update plot based on country filter
+    return render_template('visual.html', data=data, countries=categories)
 
 @app.route('/update_plot', methods=['POST'])
 def update_plot():
-    df, _ = data_render(to_dict=False)
     selected_country = request.form['country']
-    
-    if selected_country:
-        filtered_df = df[df['Country'] == selected_country]
-    else:
-        filtered_df = df
+    df = load_data()
+    filtered_df = filter_data(session["username"], df, selected_country)
     
     trace = {
         'x': filtered_df['Date'].tolist(),
@@ -105,7 +77,6 @@ def update_plot():
     }
     
     return jsonify(trace)
-
 
 @app.route('/logout', methods=['GET'])
 def logout():
