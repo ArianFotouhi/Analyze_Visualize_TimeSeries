8a60dc6a9cc0363ca6f5b2628ff6e1f1e020a1d9
Arian <78182876+ArianFotouhi@users.noreply.github.com>
2023-06-05 20:18:59 -0400

    v1.2

--- app.py
+++ app.py
@@ -4,3 +4,14 @@ import pandas as pd
 
 app = Flask(__name__)
 app.secret_key = "!241$gc"
+
+users = {
+    "user1":"pass1",
+    "user2":"pass2"
+}
+
+def authenticate(username, password):
+    if username in users and users[username] == password:
+        return True
+    return False
+
