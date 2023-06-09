96bfffdcaffa2e04bd62391b52e516caceae0cfe
Arian <78182876+ArianFotouhi@users.noreply.github.com>
2023-06-09 12:55:42 -0400

    v4.0.0
    it has flexible columns

--- app.py
+++ app.py
@@ -5,6 +5,15 @@ from datetime import datetime, timedelta
 app = Flask(__name__)
 app.secret_key = "!241$gc"
 
+
+Date_col ='Date'
+CLName_Col = 'ClientID'
+# LoungNm_Col = 'Lounge_ID'
+Lounge_ID_Col = 'Lounge_ID'
+Volume_ID_Col = 'Volume'
+
+
+
 users = {
     "admin": {"password": "admin", "ClientID": 'admin', 'AccessCL':['1','2','3','4','5','6','7','8','9','10']},
 
@@ -19,6 +28,9 @@ users = {
     "user9": {"password": "pass", "ClientID": 9 , 'AccessCL':['9']},
     "user10": {"password": "pass", "ClientID": 10 , 'AccessCL':['10']},
 }
+
+
+
 
 def authenticate(username, password):
     if username in users and users[username]["password"] == password:
@@ -43,57 +55,199 @@ def filter_data(username, df, selected_client, access_clients):
         if selected_client in access_clients:
             
         # filtering of client on upper left of screen
-            filtered_df = df[df['ClientID'] == int(selected_client)]
+            filtered_df = df[df[CLName_Col] == int(selected_client)]
         else:
             filtered_df = None
 
-    elif users[username]["ClientID"] == 'admin':
+    elif users[username]['ClientID'] == 'admin':
         
         return df
     else:
        
-        filtered_df = df[df['ClientID'] == users[username]["ClientID"]]
+        filtered_df = df[df[CLName_Col] == users[username]['ClientID']]
        
     return filtered_df
 
 def LoungeCounter(client_id):
     df = load_data()
-    unique_count = df.loc[df['ClientID'] == client_id, 'Lounge_ID'].nunique()
+    unique_count = df.loc[df[CLName_Col] == client_id, Lounge_ID_Col].nunique()
     return unique_count
 
-def stream_on_off():
+import pandas as pd
+from datetime import datetime, timedelta
+
+def stream_on_off(scale='sec', length=10):
     no_data = {}
     df = load_data()
-    unique_ids = df['ClientID'].nunique()
+    unique_ids = df[CLName_Col].nunique()
 
-    for i in range(1,unique_ids+1):                                     
-    
+    for i in range(1, unique_ids+1):
         # Assuming the 'time' column is of type datetime
-        df['INTERN_CREATION_DATE'] = pd.to_datetime(df['INTERN_CREATION_DATE'])
-        
-        # Filter the DataFrame for the last record with id=X
-        last_record = df[df['ClientID'] == i].tail(1)
-        # Get the timestamp of the last record
-        last_time = last_record['INTERN_CREATION_DATE'].iat[0]
-         
-        # Calculate the time difference
-        time_diff = datetime.now() - last_time
+        df[Date_col] = pd.to_datetime(df[Date_col])
 
-        # Check if the time difference is more than 10 seconds
-        if time_diff > timedelta(seconds=10):
-           
+        # Filter the DataFrame for the last record with id=X
+        last_record = df[df[CLName_Col] == i].tail(1)
+        # Get the timestamp of the last record
+        last_time = last_record[Date_col].iat[0]
+
+        # Calculate the time difference based on the specified scale and length
+        if scale == 'sec':
+            time_diff = datetime.now() - last_time
+            threshold = timedelta(seconds=int(length))
+        elif scale == 'min':
+            time_diff = (datetime.now() - last_time) // timedelta(minutes=1)
+            threshold = int(length)
+        elif scale == 'hour':
+            time_diff = (datetime.now() - last_time) // timedelta(hours=1)
+            threshold = int(length)
+        elif scale == 'day':
+            time_diff = (datetime.now() - last_time) // timedelta(days=1)
+            threshold = int(length)
+        elif scale == 'min':
+            time_diff = (datetime.now() - last_time) // timedelta(days=30)
+            threshold = int(length)
+        elif scale == 'year':
+            time_diff = (datetime.now() - last_time) // timedelta(days=365)
+            threshold = int(length)
+        else:
+            raise ValueError("Invalid scale. Please choose one of 'sec', 'min', 'hour', 'day', 'min', or 'year'.")
+
+        # Check if the time difference is more than the specified threshold
+        if time_diff > threshold:
             no_data[i] = last_time
 
     return no_data
 
+from datetime import datetime, timedelta
+import pytz
+import pandas as pd
 
-def active_lounges_percent(clients):
-    
-    lounge_act={}
-    #act=0
-    #inact=0
-    for i in clients:
-        pass
+def get_latest_lounge_status(df, time_difference):
+    current_date = datetime.now(pytz.UTC)
+    latest_record = None
+
+    for _, group_df in df.groupby([CLName_Col, Lounge_ID_Col]):
+        group_df[Date_col] = pd.to_datetime(group_df[Date_col], format='%Y-%m-%d %H:%M:%S')
+        group_df = group_df.sort_values(Date_col, ascending=False)  # Sort by date in descending order
+        latest_date = group_df.iloc[0][Date_col]
+
+        # Ensure the latest date has the same timezone as current_date
+        if latest_date.tzinfo is None:
+            latest_date = pytz.UTC.localize(latest_date)
+        else:
+            latest_date = latest_date.astimezone(pytz.UTC)
+
+        # Calculate the difference between the current date and the latest date
+        date_diff = (current_date.date() - latest_date.date()).days
+
+        if date_diff <= time_difference:
+            latest_record = group_df.iloc[0]
+            break
+
+    return latest_record
+
+
+def convert_to_utc(date, time_difference, date_format, convert_option=None, local_timezone='EST', utc_timezone='UTC'):
+    if convert_option is None:
+        return date
+
+    local_tz = pytz.timezone(local_timezone)
+    utc_tz = pytz.timezone(utc_timezone)
+
+    if isinstance(date, pd.Timestamp):
+        # Convert pandas Timestamp to Python datetime object
+        date = date.to_pydatetime()
+
+    if convert_option == 'local':
+        # Convert system local time to UTC
+        local_dt = datetime.now(local_tz)
+        local_dt -= timedelta(days=time_difference)
+        utc_dt = local_tz.localize(local_dt).astimezone(utc_tz)
+
+    elif convert_option == 'input_data':
+        # Convert input data date from local timezone to UTC
+        local_dt = datetime.strptime(date, date_format)
+        local_dt = local_tz.localize(local_dt).astimezone(utc_tz)
+        local_dt -= timedelta(days=time_difference)
+        utc_dt = local_dt
+
+    elif convert_option == 'both':
+        # Convert both system local time and input data date from local timezone to UTC
+        system_local_dt = datetime.now(local_tz)
+        system_local_dt -= timedelta(days=time_difference)
+        system_utc_dt = local_tz.localize(system_local_dt).astimezone(utc_tz)
+
+        local_dt = datetime.strptime(date, date_format)
+        local_dt = local_tz.localize(local_dt).astimezone(utc_tz)
+        local_dt -= timedelta(days=time_difference)
+        utc_dt = local_dt
+
+        return system_utc_dt, utc_dt
+
+    return utc_dt
+
+
+def get_lounge_status(date, time_difference):
+    current_date = datetime.now(pytz.UTC)
+    date_diff = (current_date.date() - date.date()).days
+
+    if date_diff <= time_difference:
+        return 'active'
+    else:
+        return 'inactive'
+
+
+def active_inactive_lounges(clients, time_difference, date_format, convert_option=None):
+    df = load_data()
+    active_lounges = {}
+    inactive_lounges = {}
+    act_loung_num = 0
+    inact_loung_num = 0
+
+    for client_id in clients:
+        client_df = filter_data(session["username"], df, client_id, clients)
+        active_lounge_ids = set()
+        inactive_lounge_ids = set()
+
+        latest_record = get_latest_lounge_status(client_df, time_difference)
+        while latest_record is not None:
+            lounge_id = latest_record[Lounge_ID_Col]
+            received_date = latest_record[Date_col]
+            utc_date = convert_to_utc(received_date, time_difference, date_format, convert_option)
+
+            if isinstance(utc_date, tuple):
+                # Both conversion options
+                lounge_status = get_lounge_status(utc_date[0], time_difference)
+                input_data_lounge_status = get_lounge_status(utc_date[1], time_difference)
+
+                if lounge_status == 'active' or input_data_lounge_status == 'active':
+                    active_lounge_ids.add(lounge_id)
+                else:
+                    inactive_lounge_ids.add(lounge_id)
+            else:
+                # Single conversion option
+                lounge_status = get_lounge_status(utc_date, time_difference)
+
+                if lounge_status == 'active':
+                    active_lounge_ids.add(lounge_id)
+                else:
+                    inactive_lounge_ids.add(lounge_id)
+
+            # Remove the latest record from the DataFrame
+            client_df = client_df[client_df[Lounge_ID_Col] != lounge_id]
+            latest_record = get_latest_lounge_status(client_df, time_difference)
+
+        if active_lounge_ids:
+            act_loung_num += len(active_lounge_ids)
+            active_lounges[client_id] = active_lounge_ids
+
+        if inactive_lounge_ids:
+            inact_loung_num += len(inactive_lounge_ids)
+            inactive_lounges[client_id] = inactive_lounge_ids
+
+    return active_lounges, inactive_lounges, act_loung_num, inact_loung_num
+
+
 
         #how many of lounges are active:
             #if  0 < pax_count 
@@ -108,17 +262,17 @@ def active_lounges_percent(clients):
     #how many of lounges are inactive
     #percentage of active lounges
 
-def volume_rate(clients):  
-    #grab all pax of all lounges for a client in previous week
-    #grab all pax of all lounges for a client in current week
-    #percentage of them
-    pass
+# def volume_rate(clients):  
+#     #grab all pax of all lounges for a client in previous week
+#     #grab all pax of all lounges for a client in current week
+#     #percentage of them
+#     pass
 
-def active_clients_percent(clients):
-    #how many of clients have at least one active lounge
-    #all clients
-    #percentage of them
-    pass
+# def active_clients_percent(clients):
+#     #how many of clients have at least one active lounge
+#     #all clients
+#     #percentage of them
+#     pass
 
 
 
@@ -159,9 +313,10 @@ def visual():
 
     #finds the active lounges for the selected clients
     
-    lounges_percent = active_lounges_percent(access_clients)
-    volume_ratio = volume_rate(access_clients)
-    active_clients = active_clients_percent(access_clients)   
+    lounges_percent = active_inactive_lounges(access_clients,time_difference=10000, date_format='%Y-%m-%d')
+    print(lounges_percent)
+    # volume_ratio = volume_rate(access_clients)
+    # active_clients = active_clients_percent(access_clients)   
 
     return render_template('visual.html', data=data, clients=access_clients)
 
@@ -170,16 +325,16 @@ def update_plot():
     selected_client = request.form['client']
     df = load_data()
     
-    no_data_dict = stream_on_off()
+    no_data_dict = stream_on_off(scale='hour',length=1)
 
     if selected_client:
         filtered_df = filter_data(session["username"], df, selected_client, selected_client)
         lounge_num= LoungeCounter(int(selected_client))
 
         trace = {
-            'x': filtered_df['Date'].tolist(),
-            'y': filtered_df['Volume'].tolist(),
-            'text': filtered_df.apply(lambda row: f"ClientID: {row['ClientID']}<br>Low: {row['Low']}", axis=1).tolist(),
+            'x': filtered_df[Date_col].tolist(),
+            'y': filtered_df[Volume_ID_Col].tolist(),
+            'text': filtered_df.apply(lambda row: f"ClientID: {row[CLName_Col]}<br>Low: {row['Low']}", axis=1).tolist(),
             'hovertemplate': '%{text}',
             'type': 'scatter',
             'mode': 'lines',
@@ -213,8 +368,8 @@ def update_plot():
             lounge_num= LoungeCounter(int(client))
             trace = {
                 'x': client_df['Date'].tolist(),
-                'y': client_df['Volume'].tolist(),
-                'text': client_df.apply(lambda row: f"ClientID: {row['ClientID']}<br>Low: {row['Low']}", axis=1).tolist(),
+                'y': client_df[Volume_ID_Col].tolist(),
+                'text': client_df.apply(lambda row: f"ClientID: {row[CLName_Col]}<br>Low: {row['Low']}", axis=1).tolist(),
                 'hovertemplate': '%{text}',
                 'type': 'scatter',
                 'mode': 'lines',
