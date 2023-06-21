33caa01eef942305152441f582732a8864870213
Arian <78182876+ArianFotouhi@users.noreply.github.com>
2023-06-21 10:15:48 -0400

    v5.4.9

--- config.py
+++ config.py
@@ -12,9 +12,9 @@ Ratio_Col='REF2ALW'
 time_alert = 20
 crowdedness_alert = 10
 
-users = {
-    "admin": {"password": "admin", "ClientID": 'admin', 'AccessCL':['STAR_AMS']},
-}
+# users = {
+#     "admin": {"password": "admin", "ClientID": 'admin', 'AccessCL':['STAR_AMS']},
+# }
 
 # Date_col ='DATE_UTC'
 # CLName_Col = 'CLIENT_NAME'
@@ -22,10 +22,10 @@ users = {
 # Volume_ID_Col = 'COUNT_PAX_ALLOWED'
 # Refuse_Col='COUNT_PAX_REFUSED'
 # Ratio_Col='REF2ALW'
-# users = {
-#     "admin": {"password": "admin", "ClientID": 'admin', 'AccessCL':['LH','LX','MAG']},
+users = {
+    "admin": {"password": "admin", "ClientID": 'admin', 'AccessCL':['LH','LX', 'MAG', 'CDG', 'LHR', 'DCA']},
 
-#     "user1": {"password": "pass", "ClientID": 1 , 'AccessCL':['MAG','LX']},
-#     "user2": {"password": "pass", "ClientID": 2, 'AccessCL':['LH']},
-#     "user3": {"password": "pass", "ClientID": 3, 'AccessCL':['LX']},
-# }
+    "user1": {"password": "pass", "ClientID": 1 , 'AccessCL':['MAG','LX']},
+    "user2": {"password": "pass", "ClientID": 2, 'AccessCL':['LH']},
+    "user3": {"password": "pass", "ClientID": 3, 'AccessCL':['LX']},
+}
