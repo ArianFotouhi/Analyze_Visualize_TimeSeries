a32e12f0a3469237c7948d10620dc6aad41e862b
Arian <78182876+ArianFotouhi@users.noreply.github.com>
2023-06-22 17:11:09 -0400

    v5.7.0

--- static/js/script_visual.js
+++ static/js/script_visual.js
@@ -27,6 +27,9 @@ $(document).ready(function() {
         var selectedCity = $('#city_name').val();
         var selectedCountry = $('#country_name').val();
 
+        var timeAlert = $('#time_alert').val();
+        var plotInterval = $('#plt_interval').val();
+
 
         var currentDate = new Date();
         var lastUpdate = currentDate.toLocaleString();
@@ -35,12 +38,38 @@ $(document).ready(function() {
         $.ajax({
             url: '/update_plot',
             type: 'POST',
-            data: { client: selectedClient, lounge_name: selectedLounge, airport_name: selectedAirport, city_name: selectedCity, country_name: selectedCountry},
+            data: { client: selectedClient, lounge_name: selectedLounge, 
+                airport_name: selectedAirport, city_name: selectedCity, 
+                country_name: selectedCountry, time_alert: timeAlert,
+                plt_interval: plotInterval},
 
             success: function(response) {
                 var traces = response.traces;
                 var layouts = response.layouts;
                 var errors = response.errors;
+
+                var lounge_act_num = response.lounge_act_num;
+                var lounge_inact_num = response.lounge_inact_num
+                $('#act-lounge').text(lounge_act_num);
+                $('#lounge-total').text(lounge_act_num + lounge_inact_num);
+                $('#progress_bar_lounge').css('width', (lounge_act_num * 100 / (lounge_act_num + lounge_inact_num)) + '%');
+                $('#progress_bar_lounge strong').text((lounge_act_num * 100 / (lounge_act_num + lounge_inact_num)).toFixed(2) + '%');
+
+
+                var vol_curr = response.vol_curr;
+                var vol_prev = response.vol_prev
+                $('#vol_curr').text(vol_curr);
+                $('#vol_prev').text(vol_prev);
+                $('#progress_bar_vol').css('width', (vol_curr * 100 / (vol_prev)) + '%');
+                $('#progress_bar_vol strong').text((vol_curr * 100 / (vol_prev)).toFixed(2) + '%');
+
+                var active_clients_num = response.active_clients_num;
+                var inactive_clients_num = response.inactive_clients_num
+                $('#act-client').text(active_clients_num);
+                $('#total-client').text(active_clients_num + inactive_clients_num);
+                $('#progress_bar_client').css('width', (active_clients_num * 100 / (active_clients_num + inactive_clients_num)) + '%');
+                $('#progress_bar_client strong').text((active_clients_num * 100 / (active_clients_num + inactive_clients_num)).toFixed(2) + '%');
+
 
                 var chartsContainer = $('#charts-container');
                 chartsContainer.empty();
